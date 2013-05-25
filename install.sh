e5fdb5bf84720502e3cb0c7742e6f18be17941e8
JC Grubbs <jc.grubbs@devmynd.com>
2013-05-25 14:51:40 -0500

    order matters

--- install.sh
+++ install.sh
@@ -3,32 +3,32 @@ cd ~/
 rm ~/.ackrc
 ln -s ~/source/dotfiles/.ackrc .ackrc
 
+rm ~/.bash_profile
+ln -s ~/source/dotfiles/.bash_profile .bash_profile
+
 rm ~/.gemrc
 ln -s ~/source/dotfiles/.gemrc .gemrc
 
 rm ~/.gitconfig
 ln -s ~/source/dotfiles/.gitconfig .gitconfig
 
-rm ~/.gvimrc.after
-ln -s ~/source/dotfiles/.gvimrc.after .gvimrc.after
-
-rm ~/.rspec
-ln -s ~/source/dotfiles/.rspec .rspec
-
-rm ~/.vimrc.before
-ln -s ~/source/dotfiles/.vimrc.before .vimrc.before
-
-rm ~/.bash_profile
-ln -s ~/source/dotfiles/.bash_profile .bash_profile
-
 rm ~/.githelpers
 ln -s ~/source/dotfiles/.githelpers .githelpers
+
+rm ~/.gvimrc.after
+ln -s ~/source/dotfiles/.gvimrc.after .gvimrc.after
 
 rm ~/.irbrc
 ln -s ~/source/dotfiles/.irbrc .irbrc
 
-rm ~/.vimrc.after
-ln -s ~/source/dotfiles/.vimrc.after .vimrc.after
+rm ~/.rspec
+ln -s ~/source/dotfiles/.rspec .rspec
 
 rm ~/.tmux.conf
 ln -s ~/source/dotfiles/.tmux.conf .tmux.conf
+
+rm ~/.vimrc.after
+ln -s ~/source/dotfiles/.vimrc.after .vimrc.after
+
+rm ~/.vimrc.before
+ln -s ~/source/dotfiles/.vimrc.before .vimrc.before
